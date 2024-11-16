b057fe5a1379e633733ae6ccd78e493c76fc10a4
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-11-16 22:13:49 +0800

    merge fix

--- Editor/CfFeatureSettingEditor.cs
+++ Editor/CfFeatureSettingEditor.cs
@@ -26,100 +26,102 @@ namespace cfUnityEngine.Editor
 
         private List<(FeatureStr feature, BuildTargetType buildTarget)> featureBuildTargets = new();
 
-    public void CreateGUI()
-    {
-        featureBuildTargets.Clear();
-        
-        InitBuildTargetSymbols();
-        void InitBuildTargetSymbols()
+        public void CreateGUI()
         {
-            featureBuildTargets.Capacity = FEATURES.Length;
-            foreach (var feature in FEATURES)
-            {
-                featureBuildTargets.Add((feature, BuildTargetType.None));
-            }
+            featureBuildTargets.Clear();
+
+            InitBuildTargetSymbols();
 
             void InitBuildTargetSymbols()
             {
+                featureBuildTargets.Capacity = FEATURES.Length;
                 foreach (var feature in FEATURES)
                 {
                     featureBuildTargets.Add((feature, BuildTargetType.None));
                 }
 
-                foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
+                void InitBuildTargetSymbols()
                 {
-                    if (buildTarget == BuildTargetType.None) continue;
-
-                    PlayerSettings.GetScriptingDefineSymbols(buildTarget.GetNamed(), out var symbols);
-
-                    for (var i = 0; i < featureBuildTargets.Count; i++)
+                    foreach (var feature in FEATURES)
                     {
-                        var (feature, buildTargetType) = featureBuildTargets[i];
-                        if (symbols.Contains(feature))
+                        featureBuildTargets.Add((feature, BuildTargetType.None));
+                    }
+
+                    foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
+                    {
+                        if (buildTarget == BuildTargetType.None) continue;
+
+                        PlayerSettings.GetScriptingDefineSymbols(buildTarget.GetNamed(), out var symbols);
+
+                        for (var i = 0; i < featureBuildTargets.Count; i++)
                         {
-                            featureBuildTargets[i] = (feature, buildTargetType ^ buildTarget);
-                        }
-                        else
-                        {
-                            featureBuildTargets[i] = (feature, buildTargetType ^ buildTarget);
+                            var (feature, buildTargetType) = featureBuildTargets[i];
+                            if (symbols.Contains(feature))
+                            {
+                                featureBuildTargets[i] = (feature, buildTargetType ^ buildTarget);
+                            }
+                            else
+                            {
+                                featureBuildTargets[i] = (feature, buildTargetType ^ buildTarget);
+                            }
                         }
                     }
                 }
-            }
 
-            // Each editor window contains a root VisualElement object
-            VisualElement root = rootVisualElement;
-            var applyButton = new Button(() =>
-            {
-                foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
+                // Each editor window contains a root VisualElement object
+                VisualElement root = rootVisualElement;
+                var applyButton = new Button(() =>
                 {
-                    if (buildTarget == BuildTargetType.None) continue;
-                    var features = featureBuildTargets
-                        .Where(x =>
-                        {
-                            var hasFlag = ((BuildTargetType)x.buildTarget).HasFlag(buildTarget);
-                            return hasFlag;
-                        })
-                        .Select(x => x.feature).ToArray();
-                    PlayerSettings.SetScriptingDefineSymbols(buildTarget.GetNamed(), features);
-                }
-            });
-            applyButton.text = "Apply";
-            root.Add(applyButton);
-
-            var settingList = new ListView(featureBuildTargets);
-            root.Add(settingList);
-
-            settingList.makeItem = () => new ToggleButtonGroup()
-            {
-                allowEmptySelection = true,
-                isMultipleSelection = true,
-            };
-            settingList.bindItem = (ve, idx) =>
-            {
-                var toggleGroup = ve as ToggleButtonGroup;
-                if (toggleGroup == null)
-                {
-                    Debug.LogError("Toggle setting is not a toggle group");
-                    return;
-                }
-
-                foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
-                {
-                    if (buildTarget == BuildTargetType.None) continue;
-
-                    var button = new Button(() =>
+                    foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
                     {
-                        var (feature, target) = featureBuildTargets[idx];
-                        featureBuildTargets[idx] = (feature, target ^ buildTarget);
-                    });
-                    button.text = buildTarget.GetNamed().TargetName;
-                    toggleGroup.Add(button);
-                }
+                        if (buildTarget == BuildTargetType.None) continue;
+                        var features = featureBuildTargets
+                            .Where(x =>
+                            {
+                                var hasFlag = ((BuildTargetType)x.buildTarget).HasFlag(buildTarget);
+                                return hasFlag;
+                            })
+                            .Select(x => x.feature).ToArray();
+                        PlayerSettings.SetScriptingDefineSymbols(buildTarget.GetNamed(), features);
+                    }
+                });
+                applyButton.text = "Apply";
+                root.Add(applyButton);
 
-                toggleGroup.label = featureBuildTargets[idx].feature;
-                toggleGroup.value = ToggleButtonGroupState.FromEnumFlags(featureBuildTargets[idx].buildTarget);
-            };
+                var settingList = new ListView(featureBuildTargets);
+                root.Add(settingList);
+
+                settingList.makeItem = () => new ToggleButtonGroup()
+                {
+                    allowEmptySelection = true,
+                    isMultipleSelection = true,
+                };
+                settingList.bindItem = (ve, idx) =>
+                {
+                    var toggleGroup = ve as ToggleButtonGroup;
+                    if (toggleGroup == null)
+                    {
+                        Debug.LogError("Toggle setting is not a toggle group");
+                        return;
+                    }
+
+                    foreach (var buildTarget in (BuildTargetType[])Enum.GetValues(typeof(BuildTargetType)))
+                    {
+                        if (buildTarget == BuildTargetType.None) continue;
+
+                        var button = new Button(() =>
+                        {
+                            var (feature, target) = featureBuildTargets[idx];
+                            featureBuildTargets[idx] = (feature, target ^ buildTarget);
+                        });
+                        button.text = buildTarget.GetNamed().TargetName;
+                        toggleGroup.Add(button);
+                    }
+
+                    toggleGroup.label = featureBuildTargets[idx].feature;
+                    toggleGroup.value = ToggleButtonGroupState.FromEnumFlags(featureBuildTargets[idx].buildTarget);
+                };
+            }
         }
     }
 }
