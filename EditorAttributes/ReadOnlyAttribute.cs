cfaed56b55034599c52d26d8555f508d6a7b2481
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-06-07 10:40:21 +0800

    update readonly attribute

--- EditorAttributes/ReadOnlyAttribute.cs
+++ EditorAttributes/ReadOnlyAttribute.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics;
 using UnityEngine;
 
 namespace cfUnityEngine.Editor
 {
+    [Conditional("UNITY_EDITOR")]
     public class ReadOnlyAttribute: PropertyAttribute
     {
     }
