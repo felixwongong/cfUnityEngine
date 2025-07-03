4f6238c48bf6c8ab6b7059faba50516776b70718
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-07-04 01:30:00 +0800

    add scriptable loader search pattern

--- info/ValueLoader/EditorScriptableObjectLoader.cs
+++ info/ValueLoader/EditorScriptableObjectLoader.cs
@@ -16,23 +16,25 @@ namespace cfUnityEngine.Info
 {
     public abstract class ScriptableInfo<TInfo> : ScriptableObject
     {
-        public abstract List<TInfo> infos { get; }
+        public abstract List<TInfo> getInfos { get; }
     }
     
     public class EditorScriptableObjectLoader<TInfo>: IValueLoader<TInfo>
     {
         private readonly string _folderPath;
+        private readonly string _searchPattern;
 
-        public EditorScriptableObjectLoader(string folderPath)
+        public EditorScriptableObjectLoader(string folderPath, string searchPattern = "*")
         {
             _folderPath = folderPath;
+            _searchPattern = searchPattern;
         }
         
         public ObjectPool<List<TInfo>>.Handle Load(out List<TInfo> values)
         {
             var handle = ListPool<TInfo>.Default.Get(out values);
 
-            var guids = AssetDatabase.FindAssets("*", new [] {_folderPath});
+            var guids = AssetDatabase.FindAssets(_searchPattern, new [] {_folderPath});
             foreach (var guid in guids)
             {
                 var assetPath = AssetDatabase.GUIDToAssetPath(guid);
@@ -44,10 +46,11 @@ namespace cfUnityEngine.Info
                     return handle;
                 }
 
-                if (scriptableInfo.infos != null)
+                var infos = scriptableInfo.getInfos;
+                if (infos != null)
                 {
-                    values.EnsureCapacity(values.Count + scriptableInfo.infos.Count);
-                    values.AddRange(scriptableInfo.infos);
+                    values.EnsureCapacity(values.Count + infos.Count);
+                    values.AddRange(infos);
                 }
             }
             
@@ -69,10 +72,11 @@ namespace cfUnityEngine.Info
                     continue;
                 }
 
-                if (scriptableInfo.infos != null)
+                var infos = scriptableInfo.getInfos;
+                if (infos != null)
                 {
-                    list.EnsureCapacity(list.Count + scriptableInfo.infos.Count);
-                    list.AddRange(scriptableInfo.infos);
+                    list.EnsureCapacity(list.Count + infos.Count);
+                    list.AddRange(infos);
                 }
             }
             
