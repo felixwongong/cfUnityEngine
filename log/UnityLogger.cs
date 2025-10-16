e4c5ddb8aba4ca1bfff9cd0f446f31847033629a
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-10-16 08:02:48 +0800

    add set default logger

---
+++ log/UnityLogger.cs
@@ -0,0 +1,92 @@
+using System;
+using cfEngine.Logging;
+using UnityEngine;
+using ILogger = cfEngine.Logging.ILogger;
+
+namespace cfUnityEngine.Logging
+{
+    public static class Log_SetDefault
+    {
+        [RuntimeInitializeOnLoadMethod]
+        public static void SetDefault()
+        {
+            if (!Log.hasLogger)
+                Log.SetLogger(new UnityLogger());
+        }
+    }
+    
+    public class UnityLogger : ILogger
+    {
+        public void LogDebug(string message, object context = null)
+        {
+            if (context is UnityEngine.Object unityObject)
+            {
+                Debug.Log(message, unityObject);
+            }
+            else
+            {
+                Debug.Log(message);
+            }
+        }
+
+        public void LogInfo(string message, object context = null)
+        {
+            if (context is UnityEngine.Object unityObject)
+            {
+                Debug.Log(message, unityObject);
+            }
+            else
+            {
+                Debug.Log(message);
+            }
+        }
+
+        public void Asset(bool condition, object context = null)
+        {
+            if (context != null)
+            {
+                Debug.Assert(condition);
+            }
+            else
+            {
+                Debug.Assert(condition, context);
+            }
+        }
+
+        public void LogWarning(string message, object context = null)
+        {
+            if (context is UnityEngine.Object unityObject)
+            {
+                Debug.LogWarning(message, unityObject);
+            }
+            else
+            {
+                Debug.LogWarning(message);
+            }
+        }
+
+        public void LogException(Exception ex, object context = null)
+        {
+            if (context is UnityEngine.Object unityObject)
+            {
+                Debug.LogException(ex, unityObject);
+            }
+            else
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        public void LogError(string message, object context = null)
+        {
+            if (context is UnityEngine.Object unityObject)
+            {
+                Debug.LogError(message, unityObject);
+            }
+            else
+            {
+                Debug.LogError(message);
+            }
+        }
+    }
+}
